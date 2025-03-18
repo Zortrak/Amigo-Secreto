3271b0ace21ab10d766f523cf55ae6ffafd4bdd6
Rodrigo Ortega <zortrak.o1503@gmail.com>
2025-03-18 10:34:36 -0600

    Corrección

--- challenge-amigo-secreto/app.js
+++ challenge-amigo-secreto/app.js
@@ -23,11 +23,11 @@ function agregarAmigos(){
         const listaAmigos = document.getElementById("listaAmigos");
         listaAmigos.innerHTML = "";
 
-        for( let i = 0; <amigos.length; i++){
+        for( let i = 0; i<amigos.length; i++){
             const li = document.createElement("li");
             li.textContent = amigos[i];
-            listaAmigos.appemdChild(li);
-            
+            listaAmigos.appendChild(li);
+
         }
     }
 
